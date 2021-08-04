4da2d9f675c343fc3c7ce09010a99a71295d3612
erikingebretson <erikingebretson@gmail.com>
2021-08-04 15:37:35 -0700

    syc branches

--- frontend/components/product/shoes_index/shoe_index_menu.jsx
+++ frontend/components/product/shoes_index/shoe_index_menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ShoeIndexMenu extends React.Component {
     constructor(props) {
@@ -7,10 +8,14 @@ class ShoeIndexMenu extends React.Component {
 
     render() {
         return (
-            <ul>
-                <li>I am a menu</li>
-                <li>Hear me RAWRRR</li>
-            </ul>
+            <div>
+                <h3>Home /</h3>
+                <ul>
+                    <li><Link to='/shoes'>Men's Shoes</Link></li>
+                    <li><Link to='/shoes'>Women's Shoes</Link></li>
+                </ul>
+
+            </div>
         )
     }
 }
