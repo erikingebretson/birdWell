cbfeef9f207800be14e3ae7633d09a986d96192b
erikingebretson <erikingebretson@gmail.com>
2021-08-05 15:57:31 -0700

    create product/size add toggle

--- frontend/components/product/shoes_index/shoe_grid_item.jsx
+++ frontend/components/product/shoes_index/shoe_grid_item.jsx
@@ -8,44 +8,67 @@ class ShoeGridItem extends React.Component {
     }
 
     getAllImages() {
-        // NEED TO REVISIT THIS LOGIC ONCE DB HAS BEEN SEEDED - if tempshoe.productName === this.props.shoe.productName THEN LINK THE url/img
-        console.log(this.state)
+        // filter logic to see one product grid of each shoe
+        // console.log(this.state)
+
          return this.props.allShoes.map( (tempshoe, idx) => {
              if (tempshoe.productName === this.props.shoe.productName) {
                  return <div key={idx} >
-                     {/* <Link to={`/shoes/${tempshoe.id}`} ><img className="thumbnail" src={tempshoe.photoUrls[2]} alt="" /></Link> */}
-                     <a onClick={() => this.setState({ photoUrls: tempshoe.photoUrls, id: tempshoe.id }) } ><img className="thumbnail" src={tempshoe.photoUrls[2]} alt="" /></a>
+                     <a onClick={() => this.setState({ photoUrls: tempshoe.photoUrls, id: tempshoe.id }) } >
+                         <img className="thumbnail" src={tempshoe.photoUrls[2]} alt="" />
+                    </a>
                 </div>
              }
             })
-        }
+    }
 
+    setDisplay(e) {
+        if ( e === "on" ) {
+            let ele = document.querySelector(".remove-sizes")
+            console.log(ele)
+            ele.classList.add("show-sizes")
+            ele.classList.remove("remove-sizes")
+        } else {
+            let ele = document.querySelector(".show-sizes")
+            ele.classList.add("remove-sizes")
+            ele.classList.remove("show-sizes")
+        }
+    }
 
     render() {
-        // console.log(this.state)
-        return (
-            <div>
-                <h3>{this.props.shoe.productName}</h3>
-                <p>Our everyday sneaker made with breezy eucalyptus tree.</p>
-                <div className="plp-element-root" >
-                    <div className="plp-details-box" >
-                        <Link to={`/shoes/${this.state.id}`} ><img src={this.state.photoUrls[2]} alt={this.props.shoe.productName} /></Link>
-                        <Link to={`/shoes/${this.props.shoe.id}`} >{this.props.shoe.productName}</Link>
-                        <p>{this.props.shoe.colorway}</p>
-                        <p>${this.props.shoe.price}</p>
-                        <div className="thumbnail-carousel" >
-                            {this.getAllImages()}
-                        </div>
+        
+    return (
+        <div>
+        <h3>{this.props.shoe.productName}</h3>
+        <p>{this.props.shoe.detail1}</p>
+        <div className="plp-element-root" >
+            <div className="plp-details-box" >
+                    <div className="plp-detail-container" onMouseEnter={() => this.setDisplay("on")} onMouseLeave={() => this.setDisplay("off")}>
+                    <Link to={`/shoes/${this.state.id}`} >
+                        <img src={this.state.photoUrls[2]} alt={this.props.shoe.productName} />
+                    </Link>
+                    <Link to={`/shoes/${this.props.shoe.id}`} >
+                        {this.props.shoe.productName}
+                    </Link>
+                    <p>{this.props.shoe.colorway}</p>
+                    <p>${this.props.shoe.price}</p>
+                    <div className="thumbnail-carousel" >
+                        {this.getAllImages()}
                     </div>
-                    <div className="plp-hero-img">
-                        <img src={this.props.shoe.photoUrls[6]} alt={this.props.shoe.productName}/>
-                        <div className="image-copy" >
-                            <p >Breezy Feel, Light Material</p>
-                        </div>
+                        <div className="remove-sizes"  >
+                            <p>Quick Add</p>
                     </div>
                 </div>
             </div>
-        )
+            <div className="plp-hero-img">
+                <img src={this.props.shoe.photoUrls[6]} alt={this.props.shoe.productName}/>
+                <div className="image-copy" >
+                        <p >{this.props.shoe.detail2}</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
     }       
 }
 
