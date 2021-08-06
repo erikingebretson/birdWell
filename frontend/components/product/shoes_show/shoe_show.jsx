2482e9c4e201c39ec6aec99624127471d4157ddd
erikingebretson <erikingebretson@gmail.com>
2021-08-05 18:42:49 -0700

    add plp prod description and styling

--- frontend/components/product/shoes_show/shoe_show.jsx
+++ frontend/components/product/shoes_show/shoe_show.jsx
@@ -20,20 +20,79 @@ class ShoeShow extends React.Component {
         )
     }
 
-    render() {
-        if (this.props.shoe === undefined) return null;
+    shoeColorTiles() {
 
         return (
-            <div className="shoe-show-main" >
-                <div className="shoe-grid">
-                    {this.productImages()}
+            <div className="shoe-colors">
+                <div >
+                    <button className="color-tile"></button>
                 </div>
-                <div className="product-desc" >
-                    <h3>{this.props.shoe.productName}</h3>
-                    <p>${this.props.shoe.price}</p>
-                    
+                <div >
+                    <button className="color-tile"></button>
+                </div>
+                <div >
+                    <button className="color-tile"></button>
+                </div>
+                <div >
+                    <button className="color-tile"></button>
                 </div>
             </div>
+        )
+    }
+
+    render() {
+        if (this.props.shoe === undefined) return null;
+        console.log(this.props.shoe)
+        return (
+            <div className="root">
+                <div className="pathway">
+                    <p>Home / {this.props.shoe.gender}'s Shoes </p>
+                </div>
+                <div className="shoe-show-main" >
+                    <div className="shoe-grid">
+                        {this.productImages()}
+                    </div>
+                    <div className="product-desc" >
+                        <h3>{this.props.shoe.productName}</h3>
+                        <p className="price" >${this.props.shoe.price}</p>
+                        <div className="prod-review-widget" >
+                            <p>tempreview</p>
+                        </div>
+                        <p className="colorway">CLASSICS: {this.props.shoe.colorway}</p>
+                        {this.shoeColorTiles()}
+                        <div className="size-chart" >
+                            <p>Select Size:</p>
+                            <ul>
+                                <li><button>8</button></li>
+                                <li><button>9</button></li>
+                                <li><button>10</button></li>
+                                <li><button>11</button></li>
+                                <li><button>12</button></li>
+                                <li><button>13</button></li>
+                                <li><button>14</button></li>
+                                
+                            </ul>
+                        </div>
+                        <div className="sizing-detail" >
+                            <p>This style is available in whole sizes only. In between sizes? We recommend you size down.</p>
+                        </div>
+                        <div>
+                            <button className="cart-button" >
+                                Add to Cart - ${this.props.shoe.price}
+                            </button>
+                        </div>
+                        <div className="guarantee-detail" >
+                            <p >Free shipping and 30 day returns</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="review-content">
+                    <div className="review-content-header">
+                        <h4>{this.props.shoe.gender}'s {this.props.shoe.productName} Reviews</h4>
+                    </div>
+                </div>
+            </div>
+                
         )   
     }
 }
