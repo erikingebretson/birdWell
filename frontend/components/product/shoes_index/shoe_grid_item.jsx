cb59b6f06e1452ff337ff4dc7836cfdf97319695
erikingebretson <erikingebretson@gmail.com>
2021-08-05 17:12:30 -0700

    add progress to PLP style

--- frontend/components/product/shoes_index/shoe_grid_item.jsx
+++ frontend/components/product/shoes_index/shoe_grid_item.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
 
 class ShoeGridItem extends React.Component {
@@ -23,15 +23,28 @@ class ShoeGridItem extends React.Component {
     }
 
     setDisplay(e) {
+        // if time - review for implementing react Hooks
         if ( e === "on" ) {
-            let ele = document.querySelector(".remove-sizes")
-            console.log(ele)
-            ele.classList.add("show-sizes")
-            ele.classList.remove("remove-sizes")
+
+            let ele3 = useRef(null)
+            let ele4 = useRef(null)
+
+            let ele1 = ele3.current.querySelector(".remove-sizes")
+            let ele2 = ele4.current.querySelector(".remove-left-padding")
+            
+            ele1.classList.add("show-sizes")
+            ele2.classList.add("show-left-padding")
+            ele1.classList.remove("remove-sizes")
+            ele2.classList.remove("remove-left-padding")
         } else {
-            let ele = document.querySelector(".show-sizes")
-            ele.classList.add("remove-sizes")
-            ele.classList.remove("show-sizes")
+            // let ele1 = useRef(null)
+            // let ele2 = useRef(null)
+            // ele1 = document.querySelector(".show-sizes")
+            // ele2 = document.querySelector(".show-left-padding")
+            // ele1.classList.add("remove-sizes")
+            // ele2.classList.add("remove-left-padding")
+            // ele1.classList.remove("show-sizes")
+            // ele2.classList.remove("show-left-padding")
         }
     }
 
@@ -43,7 +56,9 @@ class ShoeGridItem extends React.Component {
         <p>{this.props.shoe.detail1}</p>
         <div className="plp-element-root" >
             <div className="plp-details-box" >
-                    <div className="plp-detail-container" onMouseEnter={() => this.setDisplay("on")} onMouseLeave={() => this.setDisplay("off")}>
+                <div className="remove-left-padding" >heya</div>
+                    {/* <div className="plp-detail-container" onMouseEnter={() => this.setDisplay("on")} onMouseLeave={() => this.setDisplay("off")}> */}
+                    <div className="plp-detail-container" >
                     <Link to={`/shoes/${this.state.id}`} >
                         <img src={this.state.photoUrls[2]} alt={this.props.shoe.productName} />
                     </Link>
@@ -55,15 +70,26 @@ class ShoeGridItem extends React.Component {
                     <div className="thumbnail-carousel" >
                         {this.getAllImages()}
                     </div>
-                        <div className="remove-sizes"  >
-                            <p>Quick Add</p>
-                    </div>
+                <div className="remove-sizes">
+                    <p>Quick Add</p>
+                    <ul>
+                        <li>8</li>
+                        <li>9</li>
+                        <li>10</li>
+                        <li>11</li>
+                        <li>12</li>
+                        <li>13</li>
+                        <li>14</li>
+                    </ul>
+                </div>
                 </div>
             </div>
-            <div className="plp-hero-img">
-                <img src={this.props.shoe.photoUrls[6]} alt={this.props.shoe.productName}/>
-                <div className="image-copy" >
-                        <p >{this.props.shoe.detail2}</p>
+            <div className="hero-img-box">
+                <div className="plp-hero-img">
+                    <img src={this.props.shoe.photoUrls[6]} alt={this.props.shoe.productName}/>
+                    <div className="image-copy" >
+                            <p >{this.props.shoe.detail2}</p>
+                    </div>
                 </div>
             </div>
         </div>
