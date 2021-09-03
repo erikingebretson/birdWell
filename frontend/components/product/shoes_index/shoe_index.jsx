51df29ff1e81cbce5d68e2036fe034bb4c9b6d09
erikingebretson <erikingebretson@gmail.com>
2021-09-03 11:19:57 -0600

    setup initial index filter

--- frontend/components/product/shoes_index/shoe_index.jsx
+++ frontend/components/product/shoes_index/shoe_index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ShoeGridItem from './shoe_grid_item';
-import ShoeIndexMenu from './shoe_index_menu'
+import ShoeIndexMenu from './shoe_index_menu';
+import { Link } from 'react-router-dom';
 
 class ShoeIndex extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            allShoes: []
+            allShoes: [],
+            shoeFilter: 'all'
         }
     }
 
@@ -18,7 +20,15 @@ class ShoeIndex extends React.Component {
         let filtered = [];
         this.props.shoes.forEach((shoe, idx) => {
             if (idx % 4 === 0 && shoe.detail1 !== null) {
-                filtered.push(shoe)
+                if (this.state.shoeFilter === 'all') {
+                    filtered.push(shoe)
+                } else if (this.state.shoeFilter === 'Runners') {
+                    if (shoe.productName.includes('Runners') || shoe.productName.includes('Dashers')) {
+                        filtered.push(shoe)
+                    }
+                } else if (shoe.productName.includes(this.state.shoeFilter)) {
+                    filtered.push(shoe)
+                }
             }
         })
         
@@ -39,10 +49,39 @@ class ShoeIndex extends React.Component {
         
     }
 
-    indexMenu() {
-        return (
-            <ShoeIndexMenu path={this.props.path} />
-        )
+    // buildGrid() {
+        // return this.props.shoes.map( (shoe, idx) => {
+        //     if (idx % 4 === 0 && shoe.detail1 !== null) {
+        //         return <ShoeGridItem key={idx} shoe={shoe} allShoes={this.props.shoes} />
+        //    }
+        // })
+    // }
+
+    // indexMenu() {
+    //     return (
+    //         <ShoeIndexMenu path={this.props.path} />
+    //     )
+    // }
+    createNav() {
+        if (this.props.path === '/shoes') {
+            return <ul className="menu-nav" >
+                <li className="selected-cat"><Link to='/shoes'>New Arrivals</Link></li>
+                <li><Link to='/shoes/womens'>Women's Shoes</Link></li>
+                <li><Link to='/shoes/mens'>Men's Shoes</Link></li>
+            </ul>
+        } else if (this.props.path === '/shoes/mens') {
+            return <ul className="menu-nav" >
+                <li className="selected-cat"><Link to='/shoes/mens'>Men's Shoes</Link></li>
+                <li><Link to='/shoes/womens'>Women's Shoes</Link></li>
+                <li ><Link to='/shoes'>New Arrivals</Link></li>
+            </ul>
+        } else if (this.props.path === '/shoes/womens') {
+            return <ul className="menu-nav" >
+                <li className="selected-cat"><Link to='/shoes/womens'>Women's Shoes</Link></li>
+                <li ><Link to='/shoes/mens'>Men's Shoes</Link></li>
+                <li ><Link to='/shoes'>New Arrivals</Link></li>
+            </ul>
+        }
     }
 
 
@@ -53,11 +92,25 @@ class ShoeIndex extends React.Component {
             <div className="main-root">
                 <div className="plp-page-root" >
                     <div className="plp-menu" >
-                        {this.indexMenu()}
+                        {/* {this.indexMenu()} */}
+                        <div>
+                            <h3 className="home"><Link to='/'>Home</Link> / {this.props.path === '/shoes' ? 'New Arrivals' : this.props.path === '/shoes/mens' ? "Men's Shoes" : "Women's Shoes"} </h3>
+                            {this.createNav()}
+                            <br />
+                            <h3 className="filter-prod" >Filter By:</h3>
+                            <ul>
+                                <li><button onClick={() => this.setState({ shoeFilter: 'Runners' })}>Running</button></li>
+                                <li><button onClick={() => this.setState({ shoeFilter: 'Loungers' })}>Casual</button></li>
+                                <li><button onClick={() => this.setState({ shoeFilter: 'Wool' })}>Wool</button></li>
+                                {/* <li>Coming soon...</li> */}
+                            </ul>
+
+                        </div>
+
                     </div>
                     <div className="plp-product-root">
-                        {/* {this.buildGrid()} */}
                         {this.shoeFilter()}
+                        {/* {this.buildGrid()} */}
                     </div>
                 </div>
             </div>
